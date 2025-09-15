aa6284c28f56766d90f618434f4994d459bd641a
Burn948 <74354110+Burn948@users.noreply.github.com>
2025-09-15 21:01:11 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import logoImage from '../../images/public/logo-512.png';
+import logoImage from '../../public/images/logo-512.png';
 
 const Navigation = () => {
   const location = useLocation();
